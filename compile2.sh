d280e1e2e0e35c0a9e9d74432eab901b32dfed2e
Peter Szabo <pts@fazekas.hu>
2023-02-17 03:35:06 +0100

    added hello.c, compile.sh, compile2.sh

---
+++ compile2.sh
@@ -0,0 +1,19 @@
+#! /bin/sh --
+# by pts@fazekas.hu at Wed Feb 15 14:51:30 CET 2023
+#
+# Uses: clang version 3.8.1-24, MacOSX10.10.sdk.
+#
+# TODO(pts): Add libgcc.a.
+set -ex
+
+TOOLCHAINBASE=pts_osxcross_10.10
+
+TOOLCHAINPREFIX="$TOOLCHAINBASE"/pathbin/x86_64-apple-darwin14-
+"$TOOLCHAINPREFIX"gcc -mmacosx-version-min=10.5 -W -Wall -O2 -o hello.darwin64 hello.c
+"$TOOLCHAINPREFIX"strip hello.darwin64
+
+TOOLCHAINPREFIX="$TOOLCHAINBASE"/pathbin/i386-apple-darwin14-
+"$TOOLCHAINPREFIX"gcc -mmacosx-version-min=10.5 -W -Wall -O2 -o hello.darwin32 hello.c
+"$TOOLCHAINPREFIX"strip hello.darwin32
+
+: "$0" OK.
---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdarg.h>  /* Just to test that it works. */
+
+int main(int argc, char **argv) {
+  (void)argc; (void)argv;
+  printf("Hello, World!\n");
+  return 0;
+}
